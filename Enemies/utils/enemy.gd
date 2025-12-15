c8b61b573b3b973ceed5bfbf57101cec203162d6
Blake-Cash <ratherinn@gmail.com>
2025-12-14 22:26:10 -0800

    sprites for auto dialogue

--- Enemies/utils/enemy.gd
+++ Enemies/utils/enemy.gd
@@ -17,4 +17,4 @@ func TakeDamage(_damage: int, source_position: Vector2):
 	knockback_direction = (global_position - source_position).normalized()
 	knockback = knockback + 100 # Base on damage, or some knockback mult based on attack
 	BaseNode.move_and_slide()
-	Hud.ShowCaption.emit('You beat his ass bro')
+	Hud.ShowCaption.emit('You beat his ass bro', "Jailer One")
--- Interface/hud.gd
+++ Interface/hud.gd
@@ -1,6 +1,10 @@
 class_name HudClass extends CanvasLayer
 
 @onready var label: Label = $TopCaption/Label
+@onready var sprite: Sprite2D = $TopCaption/Sprite2D
+
+const JAILER_ONE = preload("uid://ds2n42y8kn0y8")
+const JAILER_TWO = preload("uid://36jojg0svt4n")
 
 # This will need some extension for multi-step dialogue
 signal ShowCaption(text: String, character: String)
@@ -9,10 +13,20 @@ func _ready():
 	ShowCaption.connect(RenderCaption)
 	pass
 
-func RenderCaption(text: String, _character: String = 'none') -> void:
+func RenderCaption(text: String, character: String = 'none') -> void:
 	label.text = text
+	assignSprite(character)
 	# Update sprite based on character
 	await get_tree().create_timer(2).timeout # Dynamic based on length? Give characters speaking speeds? If you voice act this, you'll 100% need dynamic lengths
 	# How do I make this timer reset if another dialogue is pushed?
 	label.text = ''
+	sprite.texture = null
 	pass
+
+func assignSprite(character: String) -> void:
+	if (character == 'Jailer One'):
+		sprite.texture = JAILER_ONE
+	elif (character == 'Jailer Two'):
+		sprite.texture = JAILER_TWO
+	else:
+		pass
