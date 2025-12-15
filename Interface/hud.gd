b94d6deb9ec533e96ab25836df90c3e5f7c54a2f
Blake-Cash <ratherinn@gmail.com>
2025-12-15 01:42:10 -0800

    tween for dialogue i guess (claude's idea)

--- Interface/hud.gd
+++ Interface/hud.gd
@@ -3,6 +3,8 @@ class_name HudClass extends CanvasLayer
 @onready var label: Label = $TopCaption/Label
 @onready var sprite: Sprite2D = $TopCaption/Sprite2D
 
+var _caption_tween: Tween
+
 const JAILER_ONE = preload("uid://ds2n42y8kn0y8")
 const JAILER_TWO = preload("uid://36jojg0svt4n")
 
@@ -15,19 +17,22 @@ func _ready():
 # Support for dialogue 'chances', where it only plays sometimes
 # This will need some extension for multi-step dialogue - Scripts?
 func RenderCaption(text: String, character: String = 'none') -> void:
+	if _caption_tween:
+		_caption_tween.kill()
+
 	label.text = text
 	assignSprite(character)
-	# Update sprite based on character
-	await get_tree().create_timer(2).timeout # Dynamic based on length? Give characters speaking speeds? If you voice act this, you'll 100% need dynamic lengths
-	# How do I make this timer reset if another dialogue is pushed?
+
+	_caption_tween = create_tween()
+	_caption_tween.tween_interval(2.0) # Dynamic based on length? Give characters speaking speeds?
+	_caption_tween.tween_callback(_clear_caption)
+
+func _clear_caption() -> void:
 	label.text = ''
 	sprite.texture = null
-	pass
 
 func assignSprite(character: String) -> void:
 	if (character == 'Jailer One'):
 		sprite.texture = JAILER_ONE
 	elif (character == 'Jailer Two'):
 		sprite.texture = JAILER_TWO
-	else:
-		pass
